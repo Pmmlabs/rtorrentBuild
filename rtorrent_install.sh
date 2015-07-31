426bd954a5598cd4b712d225670c341df369892e
pmmlabs <kilowatt36@mail.ru>
2015-07-31 16:08:00 +0300

    urls from webarchive

--- rtorrent_install.sh
+++ rtorrent_install.sh
@@ -5,8 +5,8 @@ libtorrent="libtorrent-0.13.4"
 rtorrent="rtorrent-0.9.4"
 
 wget http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.4/$sigc.tar.xz
-wget http://libtorrent.rakshasa.no/downloads/$libtorrent.tar.gz
-wget http://libtorrent.rakshasa.no/downloads/$rtorrent.tar.gz
+wget http://web.archive.org/web/20150511050113/http://libtorrent.rakshasa.no/downloads/$libtorrent.tar.gz
+wget http://web.archive.org/web/20150511050113/http://libtorrent.rakshasa.no/downloads/$rtorrent.tar.gz
 
 tar xf $sigc.tar.xz
 tar xfz $libtorrent.tar.gz
